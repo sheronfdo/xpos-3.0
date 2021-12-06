8e79271d7990338475c3522f61377ea9cebe945d
sheronfdo <jamithsheron5@gmail.com>
2021-12-07 05:02:37 +0530

    create db connection class

---
+++ XPOS 2.0/src/com/xpos/database/DbConnect.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xpos.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Jamith
+ */
+public class DbConnect {
+    static Connection con;
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        connect();
+    }
+    
+    public static void connect() throws ClassNotFoundException,SQLException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/xpos_db","root","Jamith@5000");
+    }
+    
+    public static void pushToDB(String sql)throws ClassNotFoundException,SQLException{
+        if(con == null){
+            connect();
+        }
+        con.createStatement().executeUpdate(sql);
+    }
+    
+    public static ResultSet getFromDB(String sql)throws ClassNotFoundException,SQLException{
+        if(con == null){
+            connect();
+        }
+        return con.createStatement().executeQuery(sql);
+    }
+    
+    public static Connection getDBConnection()throws Exception{
+        if (con == null) {
+            connect();
+        }
+        return con;
+    }
+}

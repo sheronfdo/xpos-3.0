31f5b9becfcd4afce64e19a77f56cd3ba8e552cb
sheronfdo <jamithsheron5@gmail.com>
2021-12-09 19:25:05 +0530

    change database connection details and use variables for details

--- XPOS 2.0/src/com/xpos/database/DbConnect.java
+++ XPOS 2.0/src/com/xpos/database/DbConnect.java
@@ -16,13 +16,20 @@ import java.sql.SQLException;
  */
 public class DbConnect {
     static Connection con;
+    
+    private static String dbHost = "localhost";
+    private static String port = "3306";
+    private static String dbName = "xpos_db_2";
+    private static String userName = "root";
+    private static String password = "Jamith@5000";
+    
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         connect();
     }
     
     public static void connect() throws ClassNotFoundException,SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/xpos_db","root","Jamith@5000");
+        con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+port+"/"+dbName,userName,password); // database name = xpos_db_2
     }
     
     public static void pushToDB(String sql)throws ClassNotFoundException,SQLException{

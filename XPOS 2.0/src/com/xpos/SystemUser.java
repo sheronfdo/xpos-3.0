3115ff1fda19a7ff73ad0acefe203dfb37755990
sheronfdo <jamithsheron5@gmail.com>
2021-12-19 18:35:34 +0530

    start to develop user class

---
+++ XPOS 2.0/src/com/xpos/SystemUser.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xpos;
+
+/**
+ *
+ * @author Jamit
+ */
+public class SystemUser {
+    public static int userId = 1;
+}

91fa3a1d52417edc5a69908178bb9f745e7aefe5
sheronfdo <jamithsheron5@gmail.com>
2022-01-11 09:10:55 +0530

    validate process

--- XPOS 2.0/src/com/xpos/validation/Validate.java
+++ XPOS 2.0/src/com/xpos/validation/Validate.java
@@ -16,29 +16,34 @@ import java.util.regex.Pattern;
  * @author Jamit
  */
 public class Validate {
-    public static boolean isName(String in){
+
+    public static boolean isName(String in) {
         return Pattern.matches("[a-zA-Z]+", in);
     }
-    
-    public static boolean  isEmail(String email){
+
+    public static boolean isText(String in) {
+        return Pattern.matches("[a-zA-Z0-9]+", in);
+    }
+
+    public static boolean isEmail(String email) {
         String regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
-        Pattern emailPat = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+        Pattern emailPat = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = emailPat.matcher(email);
         return matcher.find();
     }
-    
-    public static boolean isTelephone(String in){
-        return in.length()==10 && in.matches("[0-9]+") && in.charAt(0)=='0';
+
+    public static boolean isTelephone(String in) {
+        return in.length() == 10 && in.matches("[0-9]+") && in.charAt(0) == '0';
     }
-    
-    public static boolean isDate(String date){
+
+    public static boolean isDate(String date) {
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         Date date2 = null;
         df.setLenient(false);
-        try{
+        try {
             date2 = df.parse(date);
             return true;
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }

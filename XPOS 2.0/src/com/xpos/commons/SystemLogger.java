478508bb1778513bfb7470962a40c1532e0d7ba4
sheronfdo <jamithsheron5@gmail.com>
2022-06-20 02:48:52 +0530

    add log4j logger system to project and create class to initlogger property configurations and create log4j property file

---
+++ XPOS 2.0/src/com/xpos/commons/SystemLogger.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xpos.commons;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+/**
+ *
+ * @author Jamith
+ */
+public class SystemLogger {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void initLogger(){
+        PropertyConfigurator.configure(new SystemLogger().getClass().getResource("log4j.properties"));
+    }
+}
